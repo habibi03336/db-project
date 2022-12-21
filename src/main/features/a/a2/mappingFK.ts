a17de9ab973ca273919b77bcda10a626e6575d80
하지훈 <jnh03336@gmail.com>
2022-12-21 15:49:34 +0900

    [BE] feat: 표준 FK를 매핑한다.

--- src/main/features/a/a2/mappingFK.ts
+++ src/main/features/a/a2/mappingFK.ts
@@ -1,3 +1,4 @@
+import fs from 'fs/promises';
 import dbClient from '../../../instance/dbClient';
 import { error, success } from '../../../lib/status';
 
@@ -6,9 +7,27 @@ export default function (ipcMain: Electron.IpcMain): void {
   const channelName = 'mappingFK';
   ipcMain.handle(channelName, async (event, arg) => {
     try {
+      const { tableName, columnName, FKname } = arg[0];
+      // update scan table file
+      const targetFilePath = `${dbClient.getFilePath()}/scanTable/${tableName}.json`;
+      const buffer = await fs.readFile(targetFilePath);
+      const tableScanData = JSON.parse(buffer.toString());
+      const targetIndex = tableScanData.columns.findIndex(
+        (column) => column.name === columnName
+      );
+      tableScanData.columns[targetIndex].fk = FKname;
+      await fs.writeFile(targetFilePath, JSON.stringify(tableScanData));
+
+      // update tableByPK file
+      const FKfilePath = `${dbClient.getFilePath()}/tableByPK.json`;
+      const buff = await fs.readFile(FKfilePath);
+      const tableByPK = JSON.parse(buff.toString());
+      tableByPK[FKname].push(tableName);
+      await fs.writeFile(FKfilePath, JSON.stringify(tableByPK));
+
       return success();
-    } catch {
-      return error('');
+    } catch (err) {
+      return error(err.message);
     }
   });
 }
