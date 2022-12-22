e2ef359ffe0811b88170a727af67d61f5cf8c0d7
Gagasbandas <kjjcds@gmail.com>
2022-12-22 23:39:54 +0900

    수정본

--- src/main/features/a/a3/modifyFeatureDataType.ts
+++ src/main/features/a/a3/modifyFeatureDataType.ts
@@ -25,42 +25,24 @@ export default function (ipcMain: Electron.IpcMain): void {
         const checkFK = `select CONSTRAINT_NAME, COLUMN_NAME, REFERENCED_TABLE_SCHEMA, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME from information_schema.KEY_COLUMN_USAGE where TABLE_NAME = '${modTable}' and COLUMN_NAME = '${colName}'`;
         const res4 = await dbClient.sql(checkFK);
 
-        // add new new column with colType with meta data from res2
+        // if it is a primary key, it can't be converted to text
+        if (res4.length !== 0) {
+          res4.forEach(async (item) => {
+            if (item.CONSTRAINT_NAME === 'PRIMARY') {
+              return error('can not convert primary key to text');
+            }
+            // drop the foreign key constraint
+            const dropFK = `ALTER TABLE ${modTable} DROP CONSTRAINT ${item.CONSTRAINT_NAME}`;
+            await dbClient.sql(dropFK);
+          });
+        }
+
+        // add new new column
         const addNewCol = `alter table ${modTable} add ${colName}_new ${colType}`;
         await dbClient.sql(addNewCol);
         // update the new column with the old column
         const updateNewCol = `UPDATE ${modTable} SET ${colName}_new = CAST(${colName} AS CHAR)`;
         await dbClient.sql(updateNewCol);
-
-        // restore the constraint if the column is a foreign key or primary key
-        res4.forEach(async (item) => {
-          const constraintName = item.CONSTRAINT_NAME;
-          const refSchema = item.REFERENCED_TABLE_SCHEMA;
-          const refTable = item.REFERENCED_TABLE_NAME;
-          const refCol = item.REFERENCED_COLUMN_NAME;
-          if (constraintName === 'PRIMARY') {
-            const addPK = `ALTER TABLE ${modTable} ADD PRIMARY KEY (${colName})`;
-            await dbClient.sql(addPK);
-          } else {
-            const restoreConstraint = `alter table ${modTable}
-              add constraint ${constraintName} foreign key (${colName}) references ${refSchema}.${refTable} (${refCol})`;
-            await dbClient.sql(restoreConstraint);
-          }
-        });
-
-        // remove the constraint about the column
-        if (res4.length !== 0) {
-          res4.forEach(async (item) => {
-            if (item.CONSTRAINT_NAME === 'PRIMARY') {
-              // remove primary key
-              const removePK = `ALTER TABLE ${modTable} DROP PRIMARY KEY`;
-              await dbClient.sql(removePK);
-            } else {
-              const deleteConstraintSQL = `ALTER TABLE ${modTable} DROP CONSTRAINT ${item.CONSTRAINT_NAME}`;
-              await dbClient.sql(deleteConstraintSQL);
-            }
-          });
-        }
 
         // drop the old column after delete constraint about it
         const dropOldCol = `ALTER TABLE ${modTable} DROP COLUMN ${colName} cascade`;
@@ -68,6 +50,18 @@ export default function (ipcMain: Electron.IpcMain): void {
         // rename the new column
         const renameNewCol = `ALTER TABLE ${modTable} CHANGE ${colName}_new ${colName} ${colType}`;
         await dbClient.sql(renameNewCol);
+
+        // restore the constraint if the column is a foreign key
+        res4.forEach(async (item) => {
+          const constraintName = item.CONSTRAINT_NAME;
+          const refSchema = item.REFERENCED_TABLE_SCHEMA;
+          const refTable = item.REFERENCED_TABLE_NAME;
+          const refCol = item.REFERENCED_COLUMN_NAME;
+          if (constraintName !== 'PRIMARY') {
+            const restoreFK = `ALTER TABLE ${modTable} ADD CONSTRAINT ${constraintName} FOREIGN KEY (${colName}) REFERENCES ${refSchema}.${refTable}(${refCol})`;
+            await dbClient.sql(restoreFK);
+          }
+        });
 
         // update the data type of the column in ATTRIBUTES_OF_TABLE and following NUMERIC_ATTRIBUTES or CATEGORIC_ATTRIBUTES
         const updateType = `update ATTRIBUTES_OF_TABLES set data_type = ${colType} where table_name = '${modTable}' and attribute_name = '${colName}'`;
@@ -85,7 +79,9 @@ export default function (ipcMain: Electron.IpcMain): void {
         const getData = `select ${colName} from ${modTable}`;
         const res3 = await DbClient.sql(getData);
         // check if all of the values are Integer
-        const isAllInteger = res3.every((item) => { return Number.isInteger(item[colName]); });
+        const isAllInteger = res3.every((item) => {
+          return Number.isInteger(item[colName]);
+        });
         // check if the data contains non-numeric values
         if (isAllInteger) {
           // check if the column is a foreign key
@@ -93,40 +89,20 @@ export default function (ipcMain: Electron.IpcMain): void {
           const checkFK = `select CONSTRAINT_NAME, COLUMN_NAME, REFERENCED_TABLE_SCHEMA, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME from information_schema.KEY_COLUMN_USAGE where TABLE_NAME = '${modTable}' and COLUMN_NAME = '${colName}'`;
           const res6 = await dbClient.sql(checkFK);
 
-          // add new new column with colType with meta data from res2
           const addNewCol = `alter table ${modTable} add ${colName}_new ${colType}`;
           await dbClient.sql(addNewCol);
           // update the new column with the old column
           const updateNewCol = `UPDATE ${modTable} SET ${colName}_new = CAST(${colName} AS INT)`;
           await dbClient.sql(updateNewCol);
 
-          // restore the constraint if the column is a foreign key or primary key
-          res6.forEach(async (item) => {
-            const constraintName = item.CONSTRAINT_NAME;
-            const refSchema = item.REFERENCED_TABLE_SCHEMA;
-            const refTable = item.REFERENCED_TABLE_NAME;
-            const refCol = item.REFERENCED_COLUMN_NAME;
-            if (constraintName === 'PRIMARY') {
-              const addPK = `ALTER TABLE ${modTable} ADD PRIMARY KEY (${colName})`;
-              await dbClient.sql(addPK);
-            } else {
-              const restoreConstraint = `alter table ${modTable}
-              add constraint ${constraintName} foreign key (${colName}) references ${refSchema}.${refTable} (${refCol})`;
-              await dbClient.sql(restoreConstraint);
-            }
-          });
-
           // remove the constraint about the column
           if (res6.length !== 0) {
             res6.forEach(async (item) => {
               if (item.CONSTRAINT_NAME === 'PRIMARY') {
-                // remove primary key
-                const removePK = `ALTER TABLE ${modTable} DROP PRIMARY KEY`;
-                await dbClient.sql(removePK);
-              } else {
-                const deleteConstraintSQL = `ALTER TABLE ${modTable} DROP CONSTRAINT ${item.CONSTRAINT_NAME}`;
-                await dbClient.sql(deleteConstraintSQL);
+                return error('can not convert primary key to numeric');
               }
+              const deleteConstraintSQL = `ALTER TABLE ${modTable} DROP CONSTRAINT ${item.CONSTRAINT_NAME}`;
+              await dbClient.sql(deleteConstraintSQL);
             });
           }
 
@@ -136,6 +112,18 @@ export default function (ipcMain: Electron.IpcMain): void {
           // rename the new column
           const renameNewCol = `ALTER TABLE ${modTable} CHANGE ${colName}_new ${colName} ${colType}`;
           await dbClient.sql(renameNewCol);
+
+          // restore the constraint if the column is a foreign key or primary key
+          res6.forEach(async (item) => {
+            const constraintName = item.CONSTRAINT_NAME;
+            const refSchema = item.REFERENCED_TABLE_SCHEMA;
+            const refTable = item.REFERENCED_TABLE_NAME;
+            const refCol = item.REFERENCED_COLUMN_NAME;
+            if (constraintName !== 'PRIMARY') {
+              const restoreFK = `ALTER TABLE ${modTable} ADD CONSTRAINT ${constraintName} FOREIGN KEY (${colName}) REFERENCES ${refSchema}.${refTable}(${refCol})`;
+              await dbClient.sql(restoreFK);
+            }
+          });
 
           // update the data type of the column in ATTRIBUTES_OF_TABLE and following NUMERIC_ATTRIBUTES or CATEGORIC_ATTRIBUTES
           const updateType = `update ATTRIBUTES_OF_TABLES set data_type = ${colType} where table_name = '${modTable}' and attribute_name = '${colName}'`;
@@ -148,8 +136,6 @@ export default function (ipcMain: Electron.IpcMain): void {
         } else {
           return error('The column contains non-numeric values');
         }
-        // shoud save the meta data of the attribute like not null, primary key, default value
-        // and then restore it after changing the data type
       }
       return success(0, 'modify succeed');
     } catch (err) {
