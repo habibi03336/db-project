5b36689a1f46f46a9b133bc3ace04ead0c0cf7db
하지훈 <jnh03336@gmail.com>
2022-12-22 10:32:52 +0900

    [BE] feat: 스캔된 테이블 목록 반환 기능

--- src/main/features/a/a3/readScanTable.ts
+++ src/main/features/a/a3/readScanTable.ts
@@ -1,3 +1,5 @@
+import fs from 'fs/promises';
+import path from 'path';
 import dbClient from '../../../instance/dbClient';
 import { error, success } from '../../../lib/status';
 
@@ -5,9 +7,12 @@ export default function (ipcMain: Electron.IpcMain): void {
   const channelName = 'readScanTable';
   ipcMain.handle(channelName, async (event, arg) => {
     try {
-      return success();
+      const scanTablePath = path.join(dbClient.getFilePath(), 'scanTable');
+      const res = await fs.readdir(scanTablePath);
+      const tableNames = res.map((elem) => path.basename(elem, '.json'));
+      return success(tableNames);
     } catch {
-      return error('');
+      return error('스캔 테이블을 읽는 중에 오류가 생겼습니다.');
     }
   });
 }
