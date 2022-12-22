e485df29742adf610923784c350ff52cfaf061a6
하지훈 <jnh03336@gmail.com>
2022-12-22 19:33:23 +0900

    [BE] fix: 누락된 기능 채널 추가

--- src/main/features/a/index.ts
+++ src/main/features/a/index.ts
@@ -3,11 +3,15 @@ import csvToTable from './a1/csvToTable';
 import readTables from './a2/readTables';
 import scanTableFeature from './a2/scanTableFeature';
 import mappingFK from './a2/mappingFK';
+import deleteFeature from './a3/deleteFeature';
 import mappingPK from './a2/mappingPK';
 import readScanTable from './a3/readScanTable';
 import readScanResult from './a3/readScanResult';
+import modifyFeatureDataType from './a3/modifyFeatureDataType';
 
 export default {
+  modifyFeatureDataType,
+  deleteFeature,
   makeConnection,
   csvToTable,
   readTables,
